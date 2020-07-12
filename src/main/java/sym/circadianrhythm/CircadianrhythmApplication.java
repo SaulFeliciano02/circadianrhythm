1162d996614cd6530bec020ba388c32c28051249
Saul Feliciano <jmartinez@intenpro.com>
2020-07-12 18:26:07 -0400

    Initial commit

---
+++ src/main/java/sym/circadianrhythm/CircadianrhythmApplication.java
@@ -0,0 +1,13 @@
+package sym.circadianrhythm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircadianrhythmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircadianrhythmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sym/circadianrhythm/CircadianrhythmApplicationTests.java
@@ -0,0 +1,13 @@
+package sym.circadianrhythm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircadianrhythmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
